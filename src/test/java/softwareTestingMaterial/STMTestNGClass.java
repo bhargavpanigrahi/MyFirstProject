bdb224eec1f47df56e56e2caa89a722dc0e3cb80
bhargavpanigrahi <p.bhargav1991@gmail.com>
2018-04-18 02:44:43 -0400

    This is an inital commit

---
+++ src/test/java/softwareTestingMaterial/STMTestNGClass.java
@@ -0,0 +1,36 @@
+package softwareTestingMaterial;
+
+import org.openqa.selenium.WebDriver;
+
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class STMTestNGClass {
+	
+	
+	public WebDriver driver;
+	 
+	  @Test
+	  public void openMyBlog() {
+	 driver.get("https://www.softwaretestingmaterial.com/");
+	  }
+	  
+	  @BeforeClass
+	  public void beforeClass() {
+	   
+		  System.setProperty("webdriver.ie.driver",
+
+					"C:\\Users\\BHPANI\\Downloads\\IEDriverServer_Win32_2.48.0\\IEDriverServer.exe");
+	   driver = new InternetExplorerDriver();
+	   
+	  }
+	 
+	  @AfterClass
+	  public void afterClass() {
+		  System.out.println("kill the browser");
+	   driver.quit();
+	  }
+
+}
